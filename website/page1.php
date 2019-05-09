f7cf3ea2a4a20c425ca5ef4ec30fd4ea3aa05997
maninderjits <maninderjit5embhy@hotmail.com>
2019-05-09 11:11:47 -0700

    initial commit

---
+++ website/page1.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html  >
+<head>
+  <!-- Site made with Mobirise Website Builder v4.9.7, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v4.9.7, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/logo2.png" type="image/x-icon">
+  <meta name="description" content="">
+  
+  <title>Create User</title>
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/dropdown/css/style.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+  
+  
+  
+</head>
+<body>
+  <section class="menu cid-qTkzRZLJNu" once="menu" id="menu1-g">
+
+    
+
+    <nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <div class="hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </button>
+        <div class="menu-logo">
+            <div class="navbar-brand">
+                <span class="navbar-logo">
+                    <a href="http://shiyancai215.com">
+                         <img src="assets/images/logo2.png" alt="Mobirise" title="" style="height: 3.8rem;">
+                    </a>
+                </span>
+                <span class="navbar-caption-wrap"><a class="navbar-caption text-white display-4" href="http://shiyancai215.com">6969</a></span>
+            </div>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav nav-dropdown nav-right" data-app-modern-menu="true"><li class="nav-item dropdown">
+                    <a class="nav-link link text-white dropdown-toggle display-4" href="index.html" data-toggle="dropdown-submenu" aria-expanded="true"><span class="mbri-globe mbr-iconfont mbr-iconfont-btn"></span>Navigate</a><div class="dropdown-menu"><a class="text-white dropdown-item display-4" href="index.html"><span class="mbri-home mbr-iconfont mbr-iconfont-btn"></span>Home</a><a class="text-white dropdown-item display-4" href="page1.html" aria-expanded="true"><span class="mbri-user mbr-iconfont mbr-iconfont-btn"></span>Create User</a></div>
+                </li></ul>
+            
+        </div>
+    </nav>
+</section>
+
+<section class="engine"><a href="https://mobirise.info/w">free html5 templates</a></section><section class="mbr-section form1 cid-rpPbrbadSx" id="form1-i">
+
+    
+
+    
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="title col-12 col-lg-8">
+                <h2 class="mbr-section-title align-center pb-3 mbr-fonts-style display-2">
+                    Enter Your Information</h2>
+                
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="media-container-column col-lg-8" data-form-type="formoid">
+                <!---Formbuilder Form--->
+                <form action="https://mobirise.com/" method="POST" class="mbr-form form-with-styler">
+                    <div class="row row-sm-offset">
+                        
+                        <div hidden="hidden" data-form-alert-danger="" class="alert alert-danger col-12">
+                        </div>
+                    </div>
+                    <div class="dragArea row row-sm-offset">
+                        <div class="col-md-4  form-group" data-for="firstName">
+                            <label for="firstNameForm" class="form-control-label mbr-fonts-style display-7">First Name</label>
+                            <input type="text" name="firstName" data-form-field="FirstName" required="required" class="form-control display-7" id="firstNameForm">
+                        </div>
+						<div class="col-md-4  form-group" data-for="lastName">
+                            <label for="lastNameForm" class="form-control-label mbr-fonts-style display-7">Last Name</label>
+                            <input type="text" name="lastName" data-form-field="LastName" required="required" class="form-control display-7" id="lastNameForm">
+                        </div>
+                        <div class="col-md-4  form-group" data-for="email">
+                            <label for="emailForm" class="form-control-label mbr-fonts-style display-7">Email</label>
+                            <input type="email" name="email" data-form-field="Email" required="required" class="form-control display-7" id="emailForm">
+                        </div>                        
+                        <br>
+						<div data-for="phone" class="col-md-4  form-group">
+                            <label for="phoneForm" class="form-control-label mbr-fonts-style display-7">Phone</label>
+                            <input type="number" name="phone" data-form-field="Phone" required="required" class="form-control display-7" id="phoneForm">
+                        </div>
+						<div class="col-md-4  form-group" data-for="address">
+                            <label for="addressForm" class="form-control-label mbr-fonts-style display-7">Address</label>
+                            <input type="text" name="address" data-form-field="Address" required="required" class="form-control display-7" id="addressForm">
+                        </div>
+                        <div class="col-md-12 input-group-btn align-center"><button type="submit" class="btn btn-primary btn-form display-4">CREATE USER</button></div>
+                    </div>
+                </form><!---Formbuilder Form--->
+            </div>
+        </div>
+    </div>
+</section>
+
+
+  <script src="assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="assets/popper/popper.min.js"></script>
+  <script src="assets/tether/tether.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/smoothscroll/smooth-scroll.js"></script>
+  <script src="assets/dropdown/js/script.min.js"></script>
+  <script src="assets/touchswipe/jquery.touch-swipe.min.js"></script>
+  <script src="assets/theme/js/script.js"></script>
+  
+  
+</body>
+</html>
