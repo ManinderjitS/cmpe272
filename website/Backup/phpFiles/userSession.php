6f35d82fbadf5b61eb45c742a01fc8e710b7e1fc
maninderjits <maninderjit5embhy@hotmail.com>
2019-05-13 22:50:10 -0700

    minor changes in userSession

--- website/Backup/phpFiles/userSession.php
+++ website/Backup/phpFiles/userSession.php
@@ -2,16 +2,19 @@
   session_start();
 
   //prevent user from accessing resources (e.g, add card) if not logged in
-  function userNotLoggedIn() {
-  	if(!isset($_SESSION['loggedin']))
-      return true;
-    return false;
+  function userLoggedIn() {
+	//echo "<h3>User not logged in function.</h3><br>";
+	$result = true;  	
+	if(!isset($_SESSION['loggedin']))
+		$result = false;		
+    return $result;
   }
 
+
   //prevent user from registering while logged in
-  function userAlreadyRegistered() {
+  /*function userAlreadyRegistered() {
   	if(isset($_SESSION['loggedin']))
       return true;//header("Location: addCard.php");
     return false;
-  }
+  }*/
 ?>
