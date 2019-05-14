843fa09a2e5ec8a2f4972d5048ea1ca7ccfb1e33
fuongbregas <fuongbregas@icloud.com>
2019-05-14 13:24:59 -0700

    Update dbConnection.php

--- website/Backup/phpFiles/dbConnection.php
+++ website/Backup/phpFiles/dbConnection.php
@@ -1,15 +1,15 @@
 <?php
+$account = "admin";
 	$servername = "caisy199437731.ipagemysql.com";
-    $username = "project";
-    $password = "123456abcd.";
-    $database = "cmpe272";
-    //print("Here i m ");
-    //Create connection
-    $conn = new mysqli($servername, $username, $password, $database);
-
-	//echo "----------->".$firstName."<br>";
-
-    //Check connection
-    if ($conn->connect_error){
-        die("Connection failed. " . $conn->connect_error);
-    }
+	$username = "project";
+	$password = "123456abcd";
+	$db = "cmpe272";
+	
+	 
+	date_default_timezone_set('America/Los_Angeles'); 
+	$mysqltime = date ("Y-m-d");
+	$conn = new mysqli($servername, $username, $password,$db);
+	if ($conn->connect_error) {
+		die("Connect Failed: " . $conn->connect_error);
+	}
+?>
