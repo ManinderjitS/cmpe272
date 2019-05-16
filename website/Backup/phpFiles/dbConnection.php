269685de051fc83548ed805d511eca2634809b5c
maninderjits <maninderjit5embhy@hotmail.com>
2019-05-16 13:46:33 -0700

    minor change in dbConnect.php

--- website/Backup/phpFiles/dbConnection.php
+++ website/Backup/phpFiles/dbConnection.php
@@ -4,10 +4,10 @@
 	$username = "project";
 	$password = "123456abcd";
 	$db = "cmpe272";
-		 
-	date_default_timezone_set('America/Los_Angeles'); 
+
+	date_default_timezone_set('America/Los_Angeles');
 	$mysqltime = date ("Y-m-d");
-	$conn = new mysqli($servername, $username, $password,$db);
+	$conn = new mysql($servername, $username, $password,$db);
 	if ($conn->connect_error) {
 		die("Connect Failed: " . $conn->connect_error);
 	}
