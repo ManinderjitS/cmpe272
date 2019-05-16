3062b0ba160068078af9e96ef07c491f1f75bf16
JackyC415 <32407740+JackyC415@users.noreply.github.com>
2019-05-16 14:49:33 -0700

    added curl

--- website/Backup/product_testing.php
+++ website/Backup/product_testing.php
@@ -9,11 +9,20 @@ require_once('dbConnection.php');
    </head>
    <body>
      <form method="get" action="">
-    		<?php
+    	<?php
  			$getProducts = mysqli_query($conn, "SELECT Weblink, ImageAddr FROM Product");
  	 		while($row = mysqli_fetch_array($getProducts)):; ?>
  	 			<a href='add_record.php?link=<?php echo $row['Weblink']; ?>' target="_blank"><img src="<?php echo $row['ImageAddr']; ?>" height="200" width="300"></a>
  			<?php endwhile;
- 		?>
+ 		  ?>
+      <?php
+      if(isset($_GET['link'])) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $_GET['link']);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_exec($ch);
+        curl_close($ch);
+      }
+       ?>
    </body>
  </html>
