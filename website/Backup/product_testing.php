471cc724b659e16b9095902467607dd3a889f1c7
JackyC415 <32407740+JackyC415@users.noreply.github.com>
2019-05-16 14:42:06 -0700

    updated wrong file

--- website/Backup/product_testing.php
+++ website/Backup/product_testing.php
@@ -1,19 +1,19 @@
 <?php
 require_once('dbConnection.php');
  ?>
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <form method="get" action="">
-   		<?php
-			$getProducts = mysqli_query($conn, "SELECT p_name, p_link, p_img FROM test_market");
-	 		while($row = mysqli_fetch_array($getProducts)):; ?>
-	 			<a href='add_record.php?link=<?php echo $row['p_link']; ?>' target="_blank"><img src="<?php echo $row['p_img']; ?>" height="200" width="300"></a>
-			<?php endwhile;
-		?>
-  </body>
-</html>
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title></title>
+   </head>
+   <body>
+     <form method="get" action="">
+    		<?php
+ 			$getProducts = mysqli_query($conn, "SELECT Weblink, ImageAddr FROM Product");
+ 	 		while($row = mysqli_fetch_array($getProducts)):; ?>
+ 	 			<a href='add_record.php?link=<?php echo $row['Weblink']; ?>' target="_blank"><img src="<?php echo $row['ImageAddr']; ?>" height="200" width="300"></a>
+ 			<?php endwhile;
+ 		?>
+   </body>
+ </html>
