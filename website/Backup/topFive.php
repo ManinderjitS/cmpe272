e3b2f10ad8a43545c30069eab71c29ffe6ab9528
Jacky Z. Chen <32407740+JackyC415@users.noreply.github.com>
2019-05-18 16:10:30 -0700

    Update topFive.php

--- website/Backup/topFive.php
+++ website/Backup/topFive.php
@@ -10,33 +10,57 @@ require_once('phpFiles/dbConnection.php');
   <body>
     <h1>Top Five Most Visited MarketPlace</h1>
     <?php
-    $topFiveMarket = mysqli_query($conn, "SELECT product_name, count(product_name) AS visits FROM UserVisit GROUP BY product_name ORDER BY visits DESC LIMIT 5");
+    $topFiveMarket = mysqli_query($conn, "SELECT product_name, count(product_name) AS visits FROM UserVisit, Product GROUP BY product_name ORDER BY visits DESC LIMIT 5");
     while($row = mysqli_fetch_array($topFiveMarket)):; ?>
-      <li><?php echo $row['product_name']; ?></li>
+      <li><?php echo $row['product_name']." (".$row['visits']." visits)"; ?></li>
+    <?php endwhile;
+    ?>
+
+    <h1>Top Five Most Visited PokerFazeBook</h1>
+    <?php
+    $topFivePoker = mysqli_query($conn, "SELECT product_name, count(product_name) AS visits FROM UserVisit,Product  WHERE product_name=Name and WebSite='pokerfazebook' GROUP BY product_name ORDER BY visits DESC LIMIT 5");
+    while($row = mysqli_fetch_array($topFivePoker)):; ?>
+      <li><?php echo $row['product_name']." (".$row['visits']." visits)"; ?></li>
+    <?php endwhile;
+    ?>
+
+    <h1>Top Five Most Visited Animatory</h1>
+    <?php
+    $topFiveAnimatory = mysqli_query($conn, "SELECT product_name, count(product_name) AS visits FROM UserVisit,Product  WHERE product_name=Name and WebSite='Animatory' GROUP BY product_name ORDER BY visits DESC LIMIT 5");
+    while($row = mysqli_fetch_array($topFiveAnimatory)):; ?>
+      <li><?php echo $row['product_name']." (".$row['visits']." visits)"; ?></li>
+    <?php endwhile;
+    ?>
+
+    <h1>Top Five Most Visited Stepic</h1>
+    <?php
+    $topFiveStepic = mysqli_query($conn, "SELECT product_name, count(product_name) AS visits FROM UserVisit,Product  WHERE product_name=Name and WebSite='Stepic' GROUP BY product_name ORDER BY visits DESC LIMIT 5");
+    while($row = mysqli_fetch_array($topFiveStepic)):; ?>
+      <li><?php echo $row['product_name']." (".$row['visits']." visits)"; ?></li>
     <?php endwhile;
     ?>
 
     <h1>Top Five Most Visited Brewgy</h1>
     <?php
-    $topFiveBrewgy = mysqli_query($conn, "SELECT product_name, count(product_name) AS visits FROM UserVisit WHERE product_name LIKE 'Brewgy%' GROUP BY product_name ORDER BY visits DESC LIMIT 5");
+    $topFiveBrewgy = mysqli_query($conn, "SELECT product_name, count(product_name) AS visits FROM UserVisit,Product  WHERE product_name=Name and WebSite='Brewgy' GROUP BY product_name ORDER BY visits DESC LIMIT 5");
     while($row = mysqli_fetch_array($topFiveBrewgy)):; ?>
-      <li><?php echo $row['product_name']; ?></li>
+      <li><?php echo $row['product_name']." (".$row['visits']." visits)"; ?></li>
     <?php endwhile;
     ?>
 
     <h1>Top Five Most Visited CloudBook</h1>
     <?php
-    $topFiveCloudBook = mysqli_query($conn, "SELECT product_name, count(product_name) AS visits FROM UserVisit WHERE product_name LIKE 'Cloudbook%' GROUP BY product_name ORDER BY visits DESC LIMIT 5");
+    $topFiveCloudBook = mysqli_query($conn, "SELECT product_name, count(product_name) AS visits FROM UserVisit,Product  WHERE product_name=Name and WebSite='CloudBook' GROUP BY product_name ORDER BY visits DESC LIMIT 5");
     while($row = mysqli_fetch_array($topFiveCloudBook)):; ?>
-      <li><?php echo $row['product_name']; ?></li>
+      <li><?php echo $row['product_name']." (".$row['visits']." visits)"; ?></li>
     <?php endwhile;
     ?>
 
     <h1>Top Five Most Visited Leap</h1>
     <?php
-    $topFiveLeap = mysqli_query($conn,"SELECT product_name, count(product_name) AS visits FROM UserVisit WHERE product_name LIKE 'Service%' GROUP BY product_name ORDER BY visits DESC LIMIT 5");
+    $topFiveLeap = mysqli_query($conn, "SELECT product_name, count(product_name) AS visits FROM UserVisit,Product  WHERE product_name=Name and WebSite='Leap' GROUP BY product_name ORDER BY visits DESC LIMIT 5");
     while($row = mysqli_fetch_array($topFiveLeap)):; ?>
-      <li><?php echo $row['product_name']; ?></li>
+      <li><?php echo $row['product_name']." (".$row['visits']." visits)"; ?></li>
     <?php endwhile;
     ?>
 
